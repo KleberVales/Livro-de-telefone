f039cec9d2a68b172794bcf4db268f7145879d78
Kleber Vales <ksvales@outlook.pt>
2024-10-11 09:40:36 -0300

    Resolucao do trabalho

--- src/livrodetelefone/Resolucao.java
+++ src/livrodetelefone/Resolucao.java
@@ -1,10 +1,24 @@
 package livrodetelefone;
 
-public class Resolucao {
+import java.util.HashMap;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Solution {
+	public static void main(String[] argh) {
+		Scanner in = new Scanner(System.in);
+		HashMap<String, String> phoneBook = new HashMap<>();
+		int n = in.nextInt();
+		in.nextLine();
+		for (int i = 0; i < n; i++) {
+			phoneBook.put(in.nextLine(), in.nextLine());
+		}
+		while (in.hasNext()) {
+			String s = in.nextLine();
+			if (phoneBook.containsKey(s)) {
+				System.out.printf("%s=%s%n", s, phoneBook.get(s));
+			} else {
+				System.out.println("Not found");
+			}
+		}
 	}
-
 }
